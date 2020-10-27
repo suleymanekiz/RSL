40e482490ec0fbe000fe498438a42039e8b3632c
suleymanekiz <suleyman.ekiz@hva.nl>
2020-10-27 21:48:26 +0100

    starringtop50.sql

--- starringtop50.sql
+++ starringtop50.sql
@@ -1,28 +1,32 @@
 ---------------------------
 STARRING
 ---------------------------
-
+/* add new column into the dataset called lexemesStarring 
+based on the dataset created previously 'coolmetamovies*/
 ALTER TABLE coolmetamovies
 ADD lexemesStarring tsvector;
 
-UPDATE coolmetamovies
-SET lexemesStarring = to_tsvector(Starring);
+UPDATE coolmetamovies /*update the dataset */
+SET lexemesStarring = to_tsvector(Starring); /*Make lexemesStarring identical to Starring */
 
 
-
+/* query a particular interest on the Starring Actor 'Depp' to see related movies */
 SELECT url from coolmetamovies
 WHERE lexemesStarring @@ to_tsquery('Depp');
 
 
-
+/* float 4 is already loaded into the dataset on the first go */
+/* Finalize the recommender system */
 UPDATE coolmetamovies
 SET rank = ts_rank(lexemesStarring,plainto_tsquery(
 (
 SELECT Title FROM coolmetamovies WHERE url = 'charlie-and-the-chocolate-factory'
 )
 ));
-
+/* create a table of 50 recommendations based upon the Starring Actor in Descending rank order.
+* The threshold is very low.*/
 CREATE table recommendationsbasedonstarrings AS
 SELECT url, rank FROM coolmetamovies WHERE rank > 0.0000000000000000000000000001 ORDER BY rank DESC limit 50;
 
+/*copy the results into a csv file within the working directory */
 \copy (SELECT * FROM recommendationsBasedOnStarrings) to '/home/pi/RSL/top50Starringrecommendations.csv' WITH csv
--- titletop50.sql
+++ titletop50.sql
@@ -1,32 +1,36 @@
-
-----------------------
+---------------------------
 TITLE
-----------------------
-
+---------------------------
+/* add new column into the dataset called lexemesTitle 
+based on the dataset created previously 'coolmetamovies*/
 ALTER TABLE coolmetamovies
 ADD lexemesTitle tsvector;
 
 
 
-UPDATE coolmetamovies
-SET lexemesTitle = to_tsvector(Title);
+UPDATE coolmetamovies /*update the dataset */
+SET lexemesTitle = to_tsvector(Title); /*Make lexemesTitle identical to Title */
 
 
-
+/* query a particular interest on 'godfather' to see related movies */
 SELECT url FROM coolmetamovies
 WHERE lexemesTitle @@ to_tsquery('godfather');
 
-
+/* float 4 is already loaded into the dataset on th first go */
+/* Finalize the recommender system */
 UPDATE coolmetamovies
 SET rank = ts_rank(lexemesTitle,plainto_tsquery(
 (
-SELECT Title FROM coolmetamovies WHERE url = 'the-godfather-part-iii'
+SELECT Title FROM coolmetamovies WHERE url = 'the-godfather-part-iii' /* select the Title from a specific movie url */
 )
 ));
 
-
+/* create a table of 50 recommendations based upon the Title in Descending rank order.
+* The threshold is very low.
+Output is also low. In total 3 recommendations given.*/
 CREATE TABLE recommendationsBAsedOnTitleField AS
 SELECT url, rank FROM coolmetamovies WHERE rank > 0.01 ORDER BY rank DESC LIMIT 50;
 
-
+/*copy the results into a csv file within the working directory */
 \copy (SELECT * FROM recommendationsBasedOnTitleField) to '/home/pi/RSL/top50Titlerecommendations.csv' WITH csv;
+
